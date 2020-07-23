dfc185037b845c1ca4e0f2e6ccf4379622a032b9
homus32 <homus32.reset@mail.ru>
2020-07-23 14:48:44 +1100

    add script

--- main.py
+++ main.py
@@ -1,21 +1,74 @@
-import requests
-from lxml import html
+import feedparser
+import re
 
+import pprint
 
-def get_current_chapter(url):
-    res = requests.get(url)
-    page = html.document_fromstring(res.text)
-    path = '/html/body/div[3]/div/div[2]/div[3]/section[1]/div[2]/div[1]/div[2]/a'
-    return page.xpath(path)[0].text.replace('\n', '').replace(' ', '', 11).strip()
+class MangaLibRSS:
 
+    def __init__(self, *watch_list):
+        self.watch_list = watch_list
+        self.last_links = {}
 
-def main():
-    print(get_current_chapter("https://mangalib.me/castle-swimmer"))
+        for link in self.watch_list:
+            if "https://mangalib.me/manga-rss/" not in link:
+                raise Exception(link + " - it is not MangaLibRSS link")
 
+        for link in self.watch_list:
+            rss = feedparser.parse(link)
+            self.last_links[rss.feed.title] = rss.entries[0].link
 
+    def get_updates(self):
+        r = {}
 
+        for link in self.watch_list:
+            rss = feedparser.parse(link)
+            title = rss.feed.title
+            item = rss.entries[0]
+            cur_link = item.link
+
+            if self.last_links[title] != cur_link:
+                r[title] = {
+                    "name": title[6:],
+                    "volume": re.search(r"/v(\d+)/", cur_link).group(1),
+                    "chapter": re.search(r"/c(\d+)", cur_link).group(1),
+                    "chapter_name": item.summary,
+                    "chapter_link": cur_link,
+                    "link": re.sub(r"/v.+/c.+", "", cur_link),
+                    "published": item.published
+                }
+
+                self.last_links[title] = cur_link
+
+        return r
+
+    def get_last(self):
+        r = {}
+
+        for link in self.watch_list:
+            rss = feedparser.parse(link)
+            title = rss.feed.title
+            item = rss.entries[0]
+            cur_link = item.link
+
+            r[title] = {
+                "name": title[6:],
+                "volume": re.search(r"/v(\d+)/", cur_link).group(1),
+                "chapter": re.search(r"/c(\d+)", cur_link).group(1),
+                "chapter_name": item.summary,
+                "chapter_link": cur_link,
+                "link": re.sub(r"/v.+/c.+", "", cur_link),
+                "published": item.published
+            }
+
+        return r
 if __name__ == '__main__':
-    main()
+    links = (
+        "https://mangalib.me/manga-rss/tapir-edge",
+        "https://mangalib.me/manga-rss/yakusoku-no-neverland",
+        "https://mangalib.me/manga-rss/dr-stone",
+        "https://mangalib.me/manga-rss/ijiranaide-nagatoro-san",
+        "https://mangalib.me/manga-rss/lv2-kara-cheat-datta-moto-yuusha-kouho-no-mattari-isekai-life",
+    )
 
-# json.dump(lst, fw, ensure_ascii=False, sort_keys=True, indent='\t', separators=(',', ':'))
-# TODO программа должна уведомлять пользователя о выходе новой главы определённой манги через панель уведомлений
+    rss = MangaLibRSS(*links)
+    pprint.pprint(rss.get_last())
